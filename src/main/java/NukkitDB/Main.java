d5d50af8129869a2932c1424519418c507aaccd5
Steve Coulter <stevecoulter99@gmail.com>
2019-12-23 17:50:05 -0600

    init Nukkit() class for api usage

--- src/main/java/NukkitDB/Main.java
+++ src/main/java/NukkitDB/Main.java
@@ -3,14 +3,22 @@ package NukkitDB;
 import cn.nukkit.plugin.PluginBase;
 
 public class Main extends PluginBase {
+
+    private NukkitDB nukkitDB;
+
     @Override
     public void onEnable() {
         this.saveDefaultConfig();
+        this.nukkitDB = new NukkitDB();
         NukkitDB.createConnection(this);
     }
 
     @Override
     public void onDisable() {
         NukkitDB.closeConnection();
+    }
+
+    public NukkitDB get() {
+        return nukkitDB;
     }
 }
